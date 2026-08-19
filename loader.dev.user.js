fc4b4b51ee02abff2a8fe6f8b86b851be3951597
Richard / Madpugs <richard@norbit.online>
2026-08-19 20:36:33 +0100

    Bump loader.dev.user.js to 7.3.5-dev

--- loader.dev.user.js
+++ loader.dev.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Realism Location Marker [DEV]
 // @namespace    https://realism-location-marker.com
-// @version      7.3.4-dev
-// @description  RLM V7.3.4 DEV with modular dropdown system and multi-language support
+// @version      7.3.5-dev
+// @description  RLM V7.3.5 DEV with modular dropdown system and multi-language support
 // @author       Richard Cameron (Madpugs) - Norbit.Online / MissionChief Unofficial Team
 // @license      GPL-3.0-or-later; https://www.gnu.org/licenses/gpl-3.0.txt
 // @copyright    Copyright (C) 2025 Norbit.Online
@@ -155,7 +155,7 @@
             buildingTypes: '/api/building-types',
             dispatchCenters: '/api/dispatch-centers'
         },
-        version: '7.3.4-dev',
+        version: '7.3.5-dev',
         status: 'Dev Testing'
     };
 
